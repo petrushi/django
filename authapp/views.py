aac1e2505b5b7fe962cc64f15534f8db7de3c310
stanislaw8 <pavlovinthesky@gmail.com>
2021-06-03 13:41:33 +0300

    added login/logout

--- authapp/views.py
+++ authapp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from authapp.forms import ShopUserLoginForm
 
 
 def login(request):
     title = 'страница входа'
 
     login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+        context = {
+            'title': title,
+            'login_form': login_form,
+        }
+
+        return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
