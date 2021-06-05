fa8bb396200db5ca71977ab43a99177a150830e5
stanislaw8 <pavlovinthesky@gmail.com>
2021-06-05 08:09:50 +0300

    fixed db categories id

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,9 +1,7 @@
+from django.core.management.base import BaseCommand
 from authapp.models import ShopUser
 import json
 import os
-
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
 
 from mainapp.models import ProductCategory, Product
 
@@ -25,17 +23,13 @@ class Command(BaseCommand):
             new_category.save()
 
         products = load_from_json('products')
-
-        Product.objects.all().delete()
         for product in products:
-            category_name = product["category"]
-            # Получаем категорию по имени
-            # _category = ProductCategory.objects.get(name=category_name)
-            # Заменяем название категории объектом
-            # product['category'] = _category
-            # new_product = Product(**product)
-            # new_product.save()
+            category_pk = product['category']
 
-        # Создаем суперпользователя при помощи менеджера модели
-        ShopUser.objects.create_superuser('admin', 'admin@geekshop.local', '123', age='23')
+            _category = ProductCategory.objects.get(pk=category_pk)
 
+            product['category'] = _category
+            new_product = Product(**product)
+            new_product.save()
+
+        ShopUser.objects.create_superuser('admin', 'admin@geekshop.local', '123', age='24')
