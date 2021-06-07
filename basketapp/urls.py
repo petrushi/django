84c92914752e1e359e7283cdb7d9815210079ba0
PavlovStas <pavlovinthesky@gmail.com>
2021-06-07 06:58:23 +0300

    made basket

---
+++ basketapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+import basketapp.views as basket
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path('', basket.basket, name='view'),
+    path('add/<int:pk>/', basket.basket_add, name='add'),
+    path('remove/<int:pk>)/', basket.basket_remove, name='remove'),
+]
--- gshop/settings.py
+++ gshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'authapp',
+    'basketapp',
 ]
 AUTH_USER_MODEL = 'authapp.ShopUser'
 
