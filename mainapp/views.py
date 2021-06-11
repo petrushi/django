a9201627d8d9cda059ef4da69fd8101066f9e8c9
PavlovStas <pavlovinthesky@gmail.com>
2021-06-11 14:10:40 +0300

    quantity and cost in menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,27 @@
 from basketapp.models import Basket
 from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
+import random
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(
+        category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
 
 
 def products(request, pk=None):
@@ -16,7 +37,8 @@ def products(request, pk=None):
             products = Product.objects.all().order_by('price')
             category = {'name': 'все'}
         else:
-            products = Product.objects.filter(category__pk=pk).order_by('price')
+            products = Product.objects.filter(
+                category__pk=pk).order_by('price')
             category = get_object_or_404(ProductCategory, pk=pk)
 
         context = {
@@ -30,10 +52,15 @@ def products(request, pk=None):
         return render(request, 'mainapp/product.html', context=context)
 
     products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
     context = {
         'links_menu': links_menu,
         'title': title,
         'products': products,
         'basket': basket,
+        'hot_product': hot_product,
+        'same_products': same_products,
     }
     return render(request, 'mainapp/products.html', context=context)
